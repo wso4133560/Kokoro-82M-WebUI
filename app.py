41c853575c269cee2641dd0b4bffac781eded04c
Neural Falcon <neuralfalconofficial@gmail.com>
2025-01-14 17:19:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,12 @@ import torch
 print("Loading model...")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using device: {device}')
-MODEL = build_model('kokoro-v0_19.pth', device)
+MODEL = build_model('./KOKORO/kokoro-v0_19.pth', device)
 print("Model loaded successfully.")
 
 
-voice_list = [
-    'af', # Default voice is a 50-50 mix of af_bella & af_sarah
-    'af_bella', 'af_sarah', 'am_adam', 'am_michael',
-    'bf_emma', 'bf_isabella', 'bm_george', 'bm_lewis',
-]
-    
+
 def tts_maker(text,voice_name="af_bella",speed = 0.8,trim=0,pad_between=0,save_path="temp.wav",remove_silence=False,minimum_silence=50):
-    # global voice_list
-    # voice_name=voice_list[1]
     audio_path=tts(MODEL,device,text,voice_name,speed=speed,trim=trim,pad_between_segments=pad_between,output_file=save_path,remove_silence=remove_silence,minimum_silence=minimum_silence)
     return audio_path
 
@@ -42,7 +35,7 @@ import gradio as gr
 
 import os
 voice_list=[]
-for i in os.listdir("./voices"):
+for i in os.listdir("./KOKORO/voices"):
     voice_list.append(i.replace(".pt",""))   
 # print(voice_list)     
 
